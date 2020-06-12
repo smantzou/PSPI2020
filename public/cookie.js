7ad3f0a3ab0f2c8c436fae6224d5d1f31e7a71dd
akritidi <akritidi@csd.auth.gr>
2020-06-12 20:53:49 +0300

    changes sign in kcal cookie

--- public/cookie.js
+++ public/cookie.js
@@ -6,20 +6,24 @@ function onLoad(){
         const signInButton = document.getElementById('signin')
         signInButton.style.display = 'none'
         const toEditLi = document.getElementById('toEdit')
-        const nameTag = document.createElement('p')
+
+        /*const nameTag = document.createElement('p')
+        nameTag.classList.add('username')*/
 
         const cookie = document.cookie
         let fields = cookie.split('=')
 
-        nameTag.innerHTML = fields[1]
+        /*nameTag.innerHTML = fields[1]
         
-        toEditLi.appendChild(nameTag)
+        toEditLi.appendChild(nameTag)*/
+        
+        /*toEditLi.classList.add('nav-item')*/
+        
 
-        
-        toEditLi.classList.add('nav-item')
         toEditLi.classList.add('dropdown')
         
         const aTag = document.createElement('a')
+        aTag.id = 'user-dropdown';
         aTag.classList.add('nav-link')
         aTag.classList.add('dropdown-toggle')
         aTag.href = '#'
@@ -27,20 +31,24 @@ function onLoad(){
         aTag.setAttribute('data-toggle','dropdown')
         aTag.setAttribute('aria-haspopup','true')
         aTag.setAttribute('aria-expanded','false')
-        
+        aTag.innerHTML = fields[1];
+
         const newDiv = document.createElement('div')
         newDiv.classList.add('dropdown-menu')
         newDiv.setAttribute('aria-labelledby','navbarDropdown')
        
         const settingsTag = document.createElement('a')
         settingsTag.classList.add('dropdown-item')
+        settingsTag.id = 'js-dropdown-item';
         settingsTag.addEventListener('click',onProfile)
         settingsTag.innerHTML = 'go to profile'
        
         const logoutTag = document.createElement('a')
         logoutTag.classList.add('dropdown-item')
+        logoutTag.id = 'js-dropdown-item';
         logoutTag.innerHTML = 'Logout'
         logoutTag.addEventListener('click',onClick)
+
 
         newDiv.appendChild(logoutTag)
 
